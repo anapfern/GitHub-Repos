47623c5d94018af3534bc7fdb98502565dd41af3
Ana Fernandes <anapaularef@live.com>
2024-08-06 18:01:42 -0300

    feat: update SearchUsersInput to reflect input in URL params

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 
 export default function App() {
-  return <Header />;
+  return (
+    <BrowserRouter>
+      <Header />;
+    </BrowserRouter>
+  );
 }
--- src/components/SearchUsersInput.tsx
+++ src/components/SearchUsersInput.tsx
@@ -1,9 +1,27 @@
 import { FaSearch } from "react-icons/fa";
+import { useSearchParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 export default function SearchUsersInput() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [query, setQuery] = useState(searchParams.get('search') || '');
+
+  useEffect(() => {
+    if (query) {
+      setSearchParams({ search: query });
+    } else {
+      setSearchParams({});
+    }
+  }, [query, setSearchParams]);
+
   return (
     <div className="border w-[41.75rem] h-[2.5rem] mt-4 mb-4 ml-5 flex gap-2 rounded items-center text-placeholder">
-      <input className="flex-grow ml-2 focus:outline-none" placeholder="Buscar usuário" />
+      <input
+        className="flex-grow ml-2 focus:outline-none"
+        placeholder="Buscar usuário"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
       <FaSearch className="mr-2" />
     </div>
   );
