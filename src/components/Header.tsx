b27d8a22bd3c9ee0557b1bfdcb1b34c7868ab368
Ana Fernandes <anapaularef@live.com>
2024-08-06 18:02:29 -0300

    feat: add responsive visibility to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import SearchUsersInput from "./SearchUsersInput";
 
 export default function Header() {
   return (
-    <div className="border-b border-borderAndLine flex justify-between">
+    <div className="border-b border-borderAndLine hidden md:flex justify-between gap-2">
       <SearchUsersInput />
       <button className="flex items-center gap-1 bg-primaryColor text-whiteBackgroundLight text-paragraphMdBold pl-4 pr-4">
         <FaRegHeart size={24} />
