c69a969fac66c519f29ac8175c4b7a5e37f28bd6
Ana Fernandes <anapaularef@live.com>
2024-08-06 18:07:02 -0300

    feat: create instructions component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
+import Instructions from "./components/Instructions";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />;
+      <Header />
+      <Instructions />
     </BrowserRouter>
   );
 }
---
+++ src/components/Instructions.tsx
@@ -0,0 +1,9 @@
+import instructionsImage from '../assets/undraw_people_search.svg'
+
+export default function Instructions() {
+  return <div className="text-greyNeutral mt-10 flex flex-col items-center">
+    <h1>Procure pelo Nome ou Nome de Usuário</h1>
+    <h5>Encontre os repositórios de algum usuário digitando no campo acima</h5>
+    <img src={instructionsImage} alt="search people image" className='mt-4 hidden md:flex'/>
+  </div>;
+}
